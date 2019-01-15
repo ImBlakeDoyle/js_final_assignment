0daac3b190e0af945b74170bb7feba0ad3acaf4c
Blake <blake.doyle@hotmail.com.au>
2019-01-15 20:08:49 +1100

    All basic backend server config completed

--- express/database/connect.js
+++ express/database/connect.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true });
-
-//allows mongoose to use promises instead of callbacks
 mongoose.Promise = global.Promise;
-
 mongoose.connection.on("error", console.log);
