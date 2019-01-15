c1b632c89770f87d8d68634495e89aabc5788231
Blake <blake.doyle@hotmail.com.au>
2019-01-15 20:49:29 +1100

    config app.js react

---
+++ react/balicomau/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Test</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
