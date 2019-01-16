dd464303b13d7d4682de2d00e4b20cace5e2e9a9
Luigino De Leon <ginodleon@gmail.com>
2019-01-17 09:35:02 +1100

    create homepage structure

--- react/balicomau/src/components/App.js
+++ react/balicomau/src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
+import HomePage from './pages/HomePage'
 
 class App extends Component {
     render() {
         return (
             <div>
-                <h1>Test</h1>
+                <HomePage />
             </div>
         );
     }
---
+++ react/balicomau/src/components/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import HeroSection from "../sections/HeroSection";
+
+class HomePage extends Component {
+    render () {
+        return (
+            <HeroSection />
+        )
+    };
+}
+
+export default HomePage;
