12a471b51b0202c2a2dee0579ae32ef542c2ba73
Blake <blake.doyle@hotmail.com.au>
2019-01-16 16:20:15 +1100

    get post method to work

--- express/routes/index.js
+++ express/routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const BookingRoutes = require("./booking_routes");
 const InquiryRoutes = require("./inquiry_routes");
 
+
 router.use("/booking", BookingRoutes);
 
-router.use("/inquiry", InquiryRoutes);
+// router.use("/inquiry", InquiryRoutes);
 
 module.exports = router;
