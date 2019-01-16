02cfaebd3de282fbd759788abd96038fc6ffbca5
Blake <blake.doyle@hotmail.com.au>
2019-01-16 15:52:52 +1100

    fix booking controller

--- express/controllers/booking_controller.js
+++ express/controllers/booking_controller.js
@@ -4,5 +4,9 @@ async function create(req, res) {
     const { name, email, guests, checkin, checkout, cost, phone, comment, stripe_id } = req.body;
     const booking = await BookingModel.create({ name, email, guests, checkin, checkout, cost, phone, comment, stripe_id});
 
-    res.redirect("/booking/confirmation");
+    res.json(body);
+}
+
+module.exports = {
+    create
 }
