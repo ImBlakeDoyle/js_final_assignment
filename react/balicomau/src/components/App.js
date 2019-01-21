f7b49d73881610a6c4be6e987fe031b3e209b7c0
Blake <blake.doyle@hotmail.com.au>
2019-01-22 10:47:40 +1100

    small changes

--- react/balicomau/src/components/App.js
+++ react/balicomau/src/components/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import BookingForm from "./forms/BookingForm";
+// import BookingForm from "./forms/BookingForm";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <h1>Test</h1>
-                <h2>Booking form</h2>
-                <BookingForm />
+                {/* <h2>Booking form</h2> */}
+                {/* <BookingForm /> */}
             </div>
         );
     }
