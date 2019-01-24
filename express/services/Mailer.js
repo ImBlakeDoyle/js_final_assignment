65a767653c1ee25635e599d143aa46e8543c4440
Carl Gray <carl@firstfnf.com>
2019-01-24 11:01:22 +1100

    changes to send html with email better styling

--- express/services/Mailer.js
+++ express/services/Mailer.js
@@ -3,18 +3,20 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SEND_GRID_KEY);
 
 class Mailer {
-    constructor(to, from, subject, text) {
+    constructor(to, from, subject, text, html) {
         this.to = to;
         this.from = from;
         this.subject = subject;
-        this.text = text; 
+        this.text = text;
+        this.html = html;
     }
     async send() {
     const response = await sgMail.send({
         to: this.to,
         from: this.from,
         subject: this.subject,
-        text: this.text
+        text: this.text,
+        html: this.html
     })
 
     return response
