b42f4320910014992ee5e46bdfb4220af42f4d7a
Blake <blake.doyle@hotmail.com.au>
2019-01-25 10:33:54 +1100

    fix material ui checkin and checkout to work with redux

--- react/balicomau/src/components/forms/fields/TextField.js
+++ react/balicomau/src/components/forms/fields/TextField.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TextField from '@material-ui/core/TextField';
 
 
-const TextInput = ({input, meta, ...other}) => {
+class TextInput extends Component {
+
+    render() {
+    const {input, meta, ...other} = this.props;
     return (
         <TextField 
             error={meta.touched && meta.error}
             helperText={meta.touched && meta.error}
-            fullWidth
+            // fullWidth
             {...input}
             {...other}
         />
-    )
+    )}
 }
 
 export default TextInput;
