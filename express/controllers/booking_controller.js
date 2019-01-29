25690c9e3e2ade84a8b2e329e4aeb3be94149a9b
Blake <blake.doyle@hotmail.com.au>
2019-01-29 15:11:57 +1100

    create an array of checkin dates booked

--- express/controllers/booking_controller.js
+++ express/controllers/booking_controller.js
@@ -16,6 +16,8 @@ async function create(req, res) {
 
     const cost = calculatePayment(days);
 
+    determineUnavailableDates();
+
     const booking = await BookingModel.create({ name, email, guests, checkin, checkout, cost, phone, comment, stripe_id})
     .catch(err => console.log(err));
 
@@ -53,7 +55,7 @@ async function create(req, res) {
     .catch(err => console.log("ERROR!!!!!", err));
 
 
-    //email sending
+    // email sending
     // const bookingemail = req.body;
 
     // //email sending booking to admin @ bali 
@@ -83,6 +85,18 @@ async function payment(req,res) {
     //     amount: 
     // });
 
+}
+
+determineUnavailableDates = async () => {
+    const invalidDates = [];
+    const results = await BookingModel.find();
+
+    results.forEach((result) => {
+        invalidDates.push(result.checkin);
+    });
+
+    // allDates.push(date);
+    // console.log(` the dates are ${allDates}`);
 }
 
 // function calculateCost(){
