d89fa23238274e68374730cc1600c3fa4dc381ae
Luigino De Leon <ginodleon@gmail.com>
2019-02-04 12:29:49 +1100

    Add changes to homepage styling

--- react/balicomau/src/components/sections/MainFeatureSection.js
+++ react/balicomau/src/components/sections/MainFeatureSection.js
@@ -6,51 +6,70 @@ import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
     divBody: {
-        height: "600px",
+        height: "350px",
         width: "auto",
-        maxWidth: "1100px",
+        // maxWidth: "1100px",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         position: "relative",
+        textAlign: "center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
         // linearGradient: "to right, rbga(255,0,0,0), rgba(255,0,0,1)"
         // background: 'linear-gradient(to right, #000000, #ffffff)'
     },
-    divImage: {
-        // height: "600px",
-        // width: "auto",
-        // maxWidth: "1100px",
+    imageBody: {
+        height: "700px",
+        width: "auto",
+        maxWidth: "1200px",
         // maskImage: "linear-gradient(to left, transparent 25%, black 75%)"
-        height: "400px",
-        width: "535px"
+
+        // side by side styling
+        // height: "400px",
+        // width: "535px"
+    },
+    imageContainer: {
+    },
+    contentContainer: {
+        backgroundColor: "rgba(255,255,255,0.85)",
+        width: "500px",
+        textAlign: "justify",
+        padding: "0px 14px"
+        // margin: "auto",
+        // position: "absolute", 
+        // top: "50%",
+        // transform: "translateY(-50%) translateX(150%)"
     }
 });
 
 class MainFeatureSection extends React.Component {
     render() {
         return (
-            <Grid container xs={12} spacing={0} justify="center">
-                <Grid item md={6}>
-                    <img src={this.props.img} className={this.props.classes.divImage}/>
-                </Grid>
-                <Grid item xs={10} md={6}>
+            // side by side
+            // <Grid container xs={12} spacing={16} justify="center">
+            //     <Grid item xs={12} md={6} className={this.props.classes.imageContainer}>
+            //         <img src={this.props.img} className={this.props.classes.imageBody}/>
+            //     </Grid>
+            //     <Grid item xs={10} md={6}>
+            //         <Typography variant="body2">
+            //             {this.props.content}
+            //         </Typography>
+            //     </Grid>
+            // </Grid>
+
+            <div className={this.props.classes.divBody} style={{backgroundImage: `url(${this.props.img})`}}>
+                <div className={this.props.classes.contentContainer}>
                     <Typography variant="body2">
                         {this.props.content}
                     </Typography>
-                </Grid>
-            </Grid>
-
-            // <>
-            // <div className={this.props.classes.divBody} style={{backgroundImage: `url(${this.props.img})`}}>
-            // </div>
-            // <div>
-            //     {this.props.content}
-            // </div>
-            // </>
+                </div>
+            </div>
 
             // div containing an image fading to the right with transparency
-            // <div>
-            //     <img src={this.props.img} className={this.props.classes.divImage}/>                
+            // <div className={this.props.classes.imageContainer}>
+            //     <img src={this.props.img} className={this.props.classes.imageBody}/>                
             // </div>
         )
     }
