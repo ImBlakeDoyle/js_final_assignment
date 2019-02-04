8cbeb8d3786a1ae2294d4f9284beb770d1e80601
Carl Gray <carl@firstfnf.com>
2019-02-04 17:49:17 +1100

    working on redirect, using redux so after payments goes to landing page

--- react/balicomau/src/reducers/index.js
+++ react/balicomau/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import bookingReducer from "./booking_reducer";
 import createInquiryReducer from "./create_inquiry_reducer";
 import submitStatusReducer from "./set_submit_status_reducer";
+import setFormOpenReducer from "./set_form_open_reducer"
 import { reducer as formReducer } from "redux-form";
 
 export default combineReducers({
     bookings: bookingReducer,
     createInquiry: createInquiryReducer,
     submitStatus: submitStatusReducer,
+    formOpen: setFormOpenReducer,
     form: formReducer
 });
